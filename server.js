120a33ae1f904e4bddaa368ea3832253e525ac4f
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-08 16:55:39 +0200

    User Profile Page

--- server.js
+++ server.js
@@ -358,6 +358,36 @@ server.post("/search-blogs-count", (req, res) => {
     });
 });
 
+server.post("/search-users", (req, res) => {
+  let { query } = req.body;
+
+  User.find({ "personal_info.username": new RegExp(query, "i") })
+    .limit(50)
+    .select(
+      "personal_info.fullname personal_info.username personal_info.profile_img -_id"
+    )
+    .then((users) => {
+      return res.status(200).json({ users });
+    })
+    .catch((err) => {
+      return res.status(500).json(err.message);
+    });
+});
+
+server.post("/get-profile", (req, res) => {
+  let { username } = req.body;
+
+  User.findOne({ "personal_info.username": username })
+    .select("-personal_info.password -google_auth -updatedAt -blogs")
+    .then((user) => {
+      return res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json(err.message);
+    });
+});
+
 server.post("/create-blog", verifyJWT, (req, res) => {
   let authorId = req.user;
   let { title, des, banner, tags, content, draft } = req.body;
