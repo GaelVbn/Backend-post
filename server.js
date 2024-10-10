4fd73f7299f9a16b1964c738cad07064831edf1a
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2024-10-10 11:51:24 +0200

    side navbar

--- server.js
+++ server.js
@@ -669,7 +669,7 @@ server.post("/get-replies", (req, res) => {
   Comment.findOne({ _id })
     .populate({
       path: "children",
-      option: {
+      options: {
         limit: maxLimit,
         skip: skip,
         sort: {
